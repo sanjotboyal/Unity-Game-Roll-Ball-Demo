cd7631859793f824508991270e9455c9ad8bb20a
Sanjot Boyal <sboyal2@uwo.ca>
2018-02-09 17:42:06 -0500

    Assignment 2 Unity Project
    Finished Assignment 2

---
+++ Assets/__Scripts/InitializeCollectibles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeCollectibles : MonoBehaviour {
+
+	public GameObject prefabCollectible;
+	public int numCollectibles;
+	private int radius; 
+
+	// Use this for initialization
+	void Start () {
+		radius = 6;
+
+		System.Random rand = new System.Random();
+		Color[] arraySelection = {new Color(0,0,1,1),new Color(0,0,0,1),new Color(0,1,0,1)};
+
+		for (int i = 0; i < numCollectibles; i++) {
+			
+			float angle = i * Mathf.PI * 2 / numCollectibles;
+			Vector3 pos = new Vector3(Mathf.Cos(angle) *radius, 1, Mathf.Sin(angle)*radius);
+			GameObject collectibleGenerated = Instantiate(prefabCollectible,pos,Quaternion.identity);
+
+			int ranChose = rand.Next (3);
+			Color ranColor = arraySelection [ranChose];
+
+			collectibleGenerated.GetComponent<MeshRenderer>().material.color = ranColor;
+
+		}
+	}
+
+}
---
+++ Assets/__Scripts/PlayerControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float speed;
+	public Text gameScore;
+
+	private Rigidbody rb;
+
+	private int points;
+
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		points = 0;
+		setScore ();
+
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+
+			if (other.GetComponent<MeshRenderer>().material.color == new Color (0, 0, 1, 1)) {
+				points += 3;
+			}
+
+
+			else if (other.GetComponent<MeshRenderer>().material.color == new Color (0, 0, 0, 1)) {
+				points += 2;
+			}
+
+			else if (other.GetComponent<MeshRenderer>().material.color == new Color (0, 1, 0, 1)) {
+				points += 1;
+			}
+
+			setScore ();
+
+		}
+	}
+
+	void setScore(){
+		gameScore.text = "Score: " + points.ToString ();
+	}
+
+}
